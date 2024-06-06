aeff2d2a932563005ec548dfcddbfa8e19cbfaef
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-05 23:35:05 -0300

    Specializing course class by creating specific properties and new classes

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -2,6 +2,30 @@ namespace Balta.ContentContext
 {
     public class Course : Content
     {
+        public Course()
+        {
+            Modules = [];
+        }
 
+        public string Tag { get; set; }
+        public IList<Module> Modules { get; set; }
+    }
+
+    public class Module
+    {
+        public Module()
+        {
+            Lectures = [];
+        }
+
+        public int Order { get; set; }
+        public string Title { get; set; }
+        public IList<Lecture> Lectures { get; set; }
+    }
+
+    public class Lecture
+    {
+        public int Order { get; set; }
+        public string Title { get; set; }
     }
 }
