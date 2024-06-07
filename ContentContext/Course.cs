167b27d7e0aef736f469b4875a020bc027119ef1
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-06-06 22:28:33 -0300

    Using enums to manage course and lecture levels

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -11,5 +11,7 @@ namespace Balta.ContentContext
 
         public string Tag { get; set; }
         public IList<Module> Modules { get; set; }
+        public int DurationInMinutes { get; set; }
+        public EContentLevel Level { get; set; }
     }
 }
---
+++ ContentContext/Enums/EContentLevel.cs
@@ -0,0 +1,10 @@
+namespace Balta.ContentContext
+{
+    public enum EContentLevel
+    {
+        Beginner = 1,
+        Fundamental = 2,
+        Intermediary = 3,
+        Advanced = 4
+    }
+}
